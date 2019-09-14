419764f38e3c5d57c93cbc228d9c24232237f2fb
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-09-14 11:47:36 +0600

    Answer,Question comp added.

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,28 @@
     </div>
     <hr />
     <div class="row">
-      <div
-        class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
-      ></div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <component :is="mode"></component>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Answer from "./components/Answer.vue";
+import Question from "./components/Question.vue";
+
 export default {
   data() {
-    return {};
+    return {
+      mode: "Question"
+    };
   },
   methods: {},
-  components: {}
+  components: {
+    Answer,
+    Question
+  }
 };
 </script>
 
