ff89103ed3793444ba923273921ec6d7cb78eae9
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-09-14 12:22:22 +0600

    Animation css added.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,13 @@
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <component :is="mode"></component>
+        <transition name="flip" mode="out-in">
+          <component
+            :is="mode"
+            @answered="answered($event)"
+            @confirmed="mode = 'Question'"
+          ></component>
+        </transition>
       </div>
     </div>
   </div>
@@ -24,11 +30,57 @@ export default {
       mode: "Question"
     };
   },
-  methods: {},
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "Answer";
+      } else {
+        this.mode = "Question";
+        alert("Wrong, try again!");
+      }
+    }
+  },
   components: {
     Answer,
     Question
   }
 };
 </script>
+
+<style>
+.flip-enter {
+  /* transform: :rotateY(0deg); */
+}
+
+.flip-enter-active {
+  animation: flip-in 0.5s ease-out forwards;
+}
+
+.flip-leave {
+  /* transform: :rotateY(0deg); */
+}
+
+.flip-leave-active {
+  animation: flip-out 0.5s ease-out forwards;
+}
+
+@keyframes flip-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
+}
+
+@keyframes flip-in {
+  from {
+    transform: rotateY(90deg);
+  }
+
+  to {
+    transform: rotateY(0deg);
+  }
+}
+</style>
 
